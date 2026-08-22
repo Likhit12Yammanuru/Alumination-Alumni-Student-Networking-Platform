858ed83da9ad1e7601bf999d81e0b5115867b605
Likhit12Yammanuru <154737203+Likhit12Yammanuru@users.noreply.github.com>
2026-08-23 00:20:47 +0530

    Add files via upload

---
+++ alumni-frontend/src/data/inboxData.js
@@ -0,0 +1,24 @@
+// Alumni/src/data/inboxData.js
+
+const alumniInboxData = [
+  {
+    userId: 1,
+    name: "User One",
+    lastMessage: "Hi, I’m interested in your profile!",
+    image: "http://localhost:5000/uploads/istockphoto-2172370243-2048x2048.jpg"
+  },
+  {
+    userId: 2,
+    name: "User Two",
+    lastMessage: "Can I ask about your current job?",
+    image: "http://localhost:5000/uploads/istockphoto-1482996923-2048x2048.jpg"
+  },
+  {
+    userId: 3,
+    name: "User Three",
+    lastMessage: "Thanks for connecting with me!",
+    image: "http://localhost:5000/uploads/istockphoto-1466995518-612x612.jpg"
+  }
+];
+
+export default alumniInboxData;
